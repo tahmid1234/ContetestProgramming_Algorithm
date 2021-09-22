4fce66bbe46305816c5c08474d1149f4399b96b9
Tahmid <tahmidislam1998@gmail.com>
2021-09-22 11:51:11 +0600

    Dp, tree, dfs

---
+++ LeetCode Algorithm Problems/124_Binary_Tree_Maximum_Path_Sum.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max_sum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+       helper(root);
+       return max_sum;
+    }
+    public int helper(TreeNode root){
+        if(root==null)
+            return 0;
+        int left = Math.max(0,helper(root.left));
+        int right = Math.max(0,helper(root.right));
+        max_sum = Math.max(max_sum, left+right+root.val);
+        return Math.max(left,right)+root.val;
+    }
+}
